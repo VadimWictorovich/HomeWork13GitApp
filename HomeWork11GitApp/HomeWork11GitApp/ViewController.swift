edb49ea775dadcf42d41af141064d1516561132a
Vadim Ignatenko <vadim.wictorowich@gmail.com>
2023-08-16 20:09:55 +0300

    Project created

---
+++ HomeWork11GitApp/HomeWork11GitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork11GitApp
+//
+//  Created by Вадим Игнатенко on 16.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
